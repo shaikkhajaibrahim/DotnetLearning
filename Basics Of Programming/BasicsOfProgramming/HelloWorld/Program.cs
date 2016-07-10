4885ca3c762849d4db1faaa27fd5173834957d98
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2016-07-10 21:46:34 +0530

    Hello World

---
+++ Basics Of Programming/BasicsOfProgramming/HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Console.Read();
+
+            int myFirstInteger;
+            myFirstInteger = 10;
+            Console.WriteLine(myFirstInteger);
+            myFirstInteger = 20;
+            Console.WriteLine(myFirstInteger);
+
+            const int myFirstConstantInteger = 10;
+            //myFirstConstantInteger = 20;
+        }
+    }
+}
