042cca25590756f1d4e2101333910f8c5c4b550e
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2016-07-30 22:41:53 +0530

    Added solution

---
+++ Batch-1/MyFirstSolution/HelloWorld/Intrest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// This program will be used to calculate intrests
+    /// </summary>
+    class Intrest
+    {
+        private double principal;
+
+        private double timeInYears;
+
+        private double rateOfIntrest;
+
+        /// <summary>
+        /// This method will be used to  calculate simple Intrest.
+        /// </summary>
+        /// <returns></returns>
+        public double SimpleIntrest()
+        {
+            //Todo: Implement this to return correct values
+            return 0.0;
+        }
+
+
+    }
+}
---
+++ Batch-1/MyFirstSolution/HelloWorld/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Student
+    {
+        //name, address, mobile no & rollNO
+        //
+
+        string name;
+
+        string address;
+
+        string mobileNo;
+
+        int rollNo;
+
+        // Pay Fees
+
+        
+    }
+}
