1233cf6fe7c0c5dc4f8cb6d7923a304e48205581
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2016-08-13 22:26:25 +0530

    mvc app added

---
+++ Batch-1/MyFirstMVCApplication/MyFirstMVCApplication/Controllers/FirstController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstMVCApplication.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: First
+        public string Index()
+        {
+            return "Hi this is my first controller";
+        }
+
+        [ActionName("pi")]
+        public ActionResult PrettyIndex(string id)
+        {
+            return RedirectToAction("Index","Home");
+            //return $"Hi {id}, This is your first controller";
+        }
+    }
+}
