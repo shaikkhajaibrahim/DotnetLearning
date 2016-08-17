43f3b310ae93a3ca13d4281e37f0a62e98211d71
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2016-08-17 20:46:59 +0530

    New changes

--- Basics Of Programming/MyFirstMVC/MyFirstMVC/Controllers/HomeController.cs
+++ Basics Of Programming/MyFirstMVC/MyFirstMVC/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace MyFirstMVC.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
             return View();
---
+++ Basics Of Programming/MyFirstMVC/MyFirstMVC/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Get|HttpVerbs.Post)]
+        [ActionName("ping")]
+        public string TestMe()
+        {
+            return "Passed Routing Test";
+        }
+
+        [NonAction]
+        public void Test()
+        {
+
+        }
+
+        //[HttpPost]
+        //public ActionResult Edit(int id)
+        //{
+        //    return RedirectToAction("Index");
+        //}
+    }
+}
