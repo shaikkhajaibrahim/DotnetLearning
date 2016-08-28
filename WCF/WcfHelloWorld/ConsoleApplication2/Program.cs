bd4d2a6380329fc0fa590020dd429130b0b9d03c
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2016-08-28 19:19:26 +0530

    new changes

---
+++ WCF/WcfHelloWorld/ConsoleApplication2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            CalculatorServiceClient client=new CalculatorServiceClient();
+            Console.WriteLine(client.Add(3, 5));
+            Console.ReadLine();
+        }
+    }
+}
--- WCF/WcfHelloWorld/WcfHelloWorld/Service1.cs
+++ WCF/WcfHelloWorld/WcfHelloWorld/Service1.cs
@@ -9,6 +9,7 @@ using WcfHelloWorld.Data;
 namespace WcfHelloWorld
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
     public class CalculatorService : ICalculatorService
     {
         //public string GetData(int value)
